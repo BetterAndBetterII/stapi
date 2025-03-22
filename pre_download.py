29ef2d1422bd9cce830cbed922591f02e96ed605
Yuzhong Zhang <141388234+BetterAndBetterII@users.noreply.github.com>
2025-03-22 21:13:38 +0800

    bge-m3

--- pre_download.py
+++ pre_download.py
@@ -1,5 +1,5 @@
 from sentence_transformers import SentenceTransformer
 
 if __name__ == "__main__":
-    model = SentenceTransformer("jinaai/jina-embeddings-v3", trust_remote_code=True)
+    model = SentenceTransformer("BAAI/bge-m3", trust_remote_code=True)
     model.encode("Hello, world!")
